4e2f2954c3289bc804629001c56fda777e90cfea
Nemanja <nemanjarogic93@gmail.com>
2021-03-04 20:29:52 +0100

    Remove unused namespaces

--- src/StructuralPatterns/Proxy/ProxyLibrary/LoggingProxyExample/ISubject.cs
+++ src/StructuralPatterns/Proxy/ProxyLibrary/LoggingProxyExample/ISubject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProxyLibrary.LoggingProxyExample
+﻿namespace ProxyLibrary.LoggingProxyExample
 {
     /// <summary>
     /// The Subject interface declares common operations for both
